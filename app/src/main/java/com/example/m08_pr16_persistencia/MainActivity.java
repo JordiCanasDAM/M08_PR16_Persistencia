3878a949657475cbd15f8453d437b66f4b4124b6
Jordi <jcanasalvarez.cf@iesesteveterradas.cat>
2022-11-07 09:51:13 +0100

    Text is being written to file

--- app/src/main/java/com/example/m08_pr16_persistencia/MainActivity.java
+++ app/src/main/java/com/example/m08_pr16_persistencia/MainActivity.java
@@ -2,7 +2,19 @@ package com.example.m08_pr16_persistencia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +22,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) {
+                try {
+
+                    //Apertura arxiu
+                    FileOutputStream fileOut = getApplicationContext().openFileOutput("dades.txt", Context.MODE_PRIVATE);
+                    OutputStreamWriter textOut = new OutputStreamWriter(fileOut);
+
+                    //Operacions arxiu
+                    textOut.write("Hello world! This is an internal storage file");
+
+                    //Tancament arxiu
+                    textOut.close();
+                    fileOut.close();
+
+                } catch (Exception e){ //Cas ERROR
+                    Toast.makeText(getApplicationContext(), "ERROR: UNABLE_TO_USE_FILE ", Toast.LENGTH_SHORT).show();
+                    Log.i("ERROR", e.getStackTrace().toString());
+                }
+            }
+        });
+
+    }// M onCreate
+
+}// C MainActivity
